09b2a72fdf007dafad4a15877dcea4f2283ef360
Rahul Baser <132384912+rahulbaser148@users.noreply.github.com>
2024-04-02 00:36:03 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,13 @@
-python3 -m venv venv
+#!/bin/bash
+
+# Set up virtual environment with Python 3.10
+python3.10 -m venv venv
 
 # Activate virtual environment
 source venv/bin/activate
 
 # Install packages from requirements.txt
-pip install -r requirement.txt
+pip install -r requirements.txt
 
 # Run Python script
 python app.py
